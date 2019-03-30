87e4b3218906c9bcee13b75972c093faf7d9810f
Mansi449 <mansi10022000@gmail.com>
2019-03-31 04:23:44 +0530

    add graphQL query to authenticate user login

--- ClaimItApp/app/src/main/java/com/iitr/mdg/claimit/LoginActivity.java
+++ ClaimItApp/app/src/main/java/com/iitr/mdg/claimit/LoginActivity.java
@@ -11,13 +11,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import javax.annotation.Nonnull;
 
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
+    private ProgressDialog progressDialog;
 
-    EditText _emailText;
+    EditText username;
     EditText _passwordText;
     Button _loginButton;
     TextView _signupLink;
@@ -26,9 +32,8 @@ public class LoginActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //ButterKnife.inject(this);
 
-        _emailText = findViewById(R.id.input_email);
+        username = findViewById(R.id.input_email);
         _passwordText = findViewById(R.id.input_password);
         _loginButton = findViewById(R.id.btn_login);
         _signupLink = findViewById(R.id.link_signup);
@@ -62,26 +67,66 @@ public class LoginActivity extends AppCompatActivity {
 
         _loginButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+        progressDialog = new ProgressDialog(LoginActivity.this,
                 R.style.AppTheme_Dark_Dialog);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Authenticating...");
         progressDialog.show();
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+        final String username = this.username.getText().toString();
+        final String password = _passwordText.getText().toString();
 
         // TODO: Implement your own authentication logic here.
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
+//        new android.os.Handler().postDelayed(
+//                new Runnable() {
+//                    public void run() {
+//                        // On complete call either onLoginSuccess or onLoginFailed
+//                        onLoginSuccess();
+//                        // onLoginFailed();
+//                        progressDialog.dismiss();
+//                    }
+//                }, 3000);
+
+        GraphQLClient.getApolloClient().query(FetchQuery.builder().build()).enqueue(new ApolloCall.Callback<FetchQuery.Data>() {
+            @Override
+            public void onResponse(@Nonnull Response<FetchQuery.Data> response) {
+                Log.e("Response from GraphQL", "onResponse: " + response.toString());
+                //Log.e("sixe: ", Integer.toString(response.data().hint_user_info.get(0).));
+                int x =0;
+
+                for (int i=0; i<response.data().hint_user_info.size(); i++){
+                    if(username.equals(response.data().hint_user_info.get(i).username)){
+                        if (password.equals(response.data().hint_user_info.get(i).password)){
+                            x=1;
+                            break;
+                        }
                     }
-                }, 3000);
+                }
+                if(x==1){
+                    LoginActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            onLoginSuccess();
+                        }
+                    });
+                }
+                else if(x==0) {
+                    LoginActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            onLoginFailed();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(@Nonnull ApolloException e) {
+                progressDialog.dismiss();
+                _loginButton.setEnabled(true);
+            }
+        });
     }
 
 
@@ -104,6 +149,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginSuccess() {
+        progressDialog.dismiss();
         _loginButton.setEnabled(true);
        // finish();
         Intent i = new Intent(this, MainActivity.class);
@@ -112,24 +158,24 @@ public class LoginActivity extends AppCompatActivity {
 
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
+        progressDialog.dismiss();
         _loginButton.setEnabled(true);
     }
 
     public boolean validate() {
         boolean valid = true;
 
-        String email = _emailText.getText().toString();
+        String uname = username.getText().toString();
         String password = _passwordText.getText().toString();
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+        if (uname.isEmpty()) {
+            username.setError("enter a valid email address");
             valid = false;
         } else {
-            _emailText.setError(null);
+            username.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+        if (password.isEmpty() || password.length() < 4 || password.length() > 15) {
             _passwordText.setError("between 4 and 10 alphanumeric characters");
             valid = false;
         } else {
