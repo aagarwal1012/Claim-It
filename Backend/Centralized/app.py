ba76dfa014af972790a0493bce0829610a7b61d5
Nikunj Gupta <nk.nikunjgupta@gmail.com>
2019-03-31 05:56:35 +0530

    Integrates the centralized and decentralized
    Signed-off-by: Nikunj Gupta <nk.nikunjgupta@gmail.com>

--- Backend/Centralized/app.py
+++ Backend/Centralized/app.py
@@ -1,47 +1,56 @@
 import os
-from flask import Flask, request, redirect, url_for
-from werkzeug.utils import secure_filename
-from flask import send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
+from werkzeug import secure_filename
 
-UPLOAD_FOLDER = '/home/nk/Projects/Claim-It/Backend/Centralized/media'
-ALLOWED_EXTENSIONS = set(['mp4', 'csv'])
+import requests
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.config['UPLOAD_FOLDER'] = 'media/'
+app.config['ALLOWED_EXTENSIONS'] = set(['mp4', 'csv'])
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-@app.route('/api/video/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+
+@app.route('/api/video/upload', methods=['POST'])
+def upload():
+    uploaded_files = request.files.getlist("file[]")
+    filenames = []
+    for file in uploaded_files:
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+            filenames.append(filename)
+            checkSum = hashlib.md5(file.read()).hexdigest()
 
-@app.route('/api/video/uploads/<filename>')
+            url = 'http://127.0.0.1/api/video/upload/'+filename
+
+            params = {
+                'url': url,
+                'hash': checkSum
+            }
+
+            r = requests.post(
+                'http://127.0.0.1/api/video_hash',
+                data=params
+            )
+            print(checkSum)
+    return jsonify(sucess=True)
+
+@app.route('/api/video/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
+@app.route('/test')
+def testing():
+    return jsonify({"Testing": "ok done!"})
+
+if __name__ == "__main__":
+    app.run(
+        host="127.0.0.1",
+        port=int("80"),
+        debug=True
+    )
